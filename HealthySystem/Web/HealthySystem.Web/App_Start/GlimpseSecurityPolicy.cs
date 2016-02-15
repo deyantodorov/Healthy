fa06cda0e46e9922cda13d26ddab1282c9e1047b
Deyan Todorov <deyan.todorov@hotmail.com>
2016-02-15 10:27:16 +0200

    Configure Glimpse security policy

---
+++ HealthySystem/Web/HealthySystem.Web/App_Start/GlimpseSecurityPolicy.cs
@@ -0,0 +1,24 @@
+namespace HealthySystem.Web
+{
+    using Glimpse.AspNet.Extensions;
+    using Glimpse.Core.Extensibility;
+
+    public class GlimpseSecurityPolicy : IRuntimePolicy
+    {
+        /// <summary>
+        /// You can perform a check like the one below to control Glimpse's permissions within your application.
+        /// More information about RuntimePolicies can be found at http://getglimpse.com/Help/Custom-Runtime-Policy
+        /// </summary>
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            var httpContext = policyContext.GetHttpContext();
+            return !httpContext.User.IsInRole("Administrator") ? RuntimePolicy.Off : RuntimePolicy.On;
+        }
+
+        /// <summary>
+        /// The RuntimeEvent.ExecuteResource is only needed in case you create a security policy
+        /// Have a look at http://blog.getglimpse.com/2013/12/09/protect-glimpse-axd-with-your-custom-runtime-policy/ for more details
+        /// </summary>
+        public RuntimeEvent ExecuteOn => RuntimeEvent.EndRequest | RuntimeEvent.ExecuteResource;
+    }
+}
