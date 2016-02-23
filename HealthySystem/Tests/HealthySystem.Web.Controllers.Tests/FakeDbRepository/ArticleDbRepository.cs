8de87461eb5bd65a5301e42f18bdb51bb1114ec6
Deyan Todorov <deyan.todorov@hotmail.com>
2016-02-23 12:25:52 +0200

    HomeControllerTests

---
+++ HealthySystem/Tests/HealthySystem.Web.Controllers.Tests/FakeDbRepository/ArticleDbRepository.cs
@@ -0,0 +1,145 @@
+﻿namespace HealthySystem.Web.Controllers.Tests.FakeDbRepository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using HealthySystem.Data.Models;
+
+    public class ArticleDbRepository
+    {
+        public static IQueryable<Article> GetArticles()
+        {
+            var articles = new List<Article>()
+            {
+                new Article()
+                {
+                    Id = 1,
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = null,
+                    IsDeleted = false,
+                    DeletedOn = null,
+                    Title = "Заглавие на статия",
+                    Alias = "zaglavie-na-statiya",
+                    Description = "Описание на статия",
+                    Content = "Съдържание на статия",
+                    IsPublished = true,
+                    PublishDate = DateTime.Now,
+                    UnpublishDate = null,
+                    Rank = 0,
+                    ImageId = null,
+                    RubricId = 1,
+                    AuthorId = "123123",
+                    Tags = new HashSet<Tag>(),
+                    Comments = new HashSet<Comment>()
+                },
+                new Article()
+                {
+                    Id = 2,
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = null,
+                    IsDeleted = false,
+                    DeletedOn = null,
+                    Title = "Заглавие на статия 2",
+                    Alias = "zaglavie-na-statiya 2",
+                    Description = "Описание на статия 2",
+                    Content = "Съдържание на статия 2",
+                    IsPublished = true,
+                    PublishDate = DateTime.Now,
+                    UnpublishDate = null,
+                    Rank = 0,
+                    ImageId = null,
+                    RubricId = 1,
+                    AuthorId = "123123",
+                    Tags = new HashSet<Tag>(),
+                    Comments = new HashSet<Comment>()
+                },
+                new Article()
+                {
+                    Id = 3,
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = null,
+                    IsDeleted = false,
+                    DeletedOn = null,
+                    Title = "Заглавие на статия ",
+                    Alias = "zaglavie-na-statiya ",
+                    Description = "Описание на статия 3",
+                    Content = "Съдържание на статия 3",
+                    IsPublished = true,
+                    PublishDate = DateTime.Now,
+                    UnpublishDate = null,
+                    Rank = 0,
+                    ImageId = null,
+                    RubricId = 1,
+                    AuthorId = "123123",
+                    Tags = new HashSet<Tag>(),
+                    Comments = new HashSet<Comment>()
+                },
+                new Article()
+                {
+                    Id = 4,
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = null,
+                    IsDeleted = false,
+                    DeletedOn = null,
+                    Title = "Заглавие на статия 4",
+                    Alias = "zaglavie-na-statiya 4",
+                    Description = "Описание на статия 4",
+                    Content = "Съдържание на статия 4",
+                    IsPublished = true,
+                    PublishDate = DateTime.Now,
+                    UnpublishDate = null,
+                    Rank = 0,
+                    ImageId = null,
+                    RubricId = 1,
+                    AuthorId = "123123",
+                    Tags = new HashSet<Tag>(),
+                    Comments = new HashSet<Comment>()
+                },
+                new Article()
+                {
+                    Id = 5,
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = null,
+                    IsDeleted = false,
+                    DeletedOn = null,
+                    Title = "Заглавие на статия 5",
+                    Alias = "zaglavie-na-statiya 5",
+                    Description = "Описание на статия 5",
+                    Content = "Съдържание на статия 5",
+                    IsPublished = true,
+                    PublishDate = DateTime.Now,
+                    UnpublishDate = null,
+                    Rank = 0,
+                    ImageId = null,
+                    RubricId = 1,
+                    AuthorId = "123123",
+                    Tags = new HashSet<Tag>(),
+                    Comments = new HashSet<Comment>()
+                },
+                new Article()
+                {
+                    Id = 6,
+                    CreatedOn = DateTime.Now,
+                    ModifiedOn = null,
+                    IsDeleted = false,
+                    DeletedOn = null,
+                    Title = "Заглавие на статия 6",
+                    Alias = "zaglavie-na-statiya 6",
+                    Description = "Описание на статия 6",
+                    Content = "Съдържание на статия 6",
+                    IsPublished = true,
+                    PublishDate = DateTime.Now,
+                    UnpublishDate = null,
+                    Rank = 0,
+                    ImageId = null,
+                    RubricId = 1,
+                    AuthorId = "123123",
+                    Tags = new HashSet<Tag>(),
+                    Comments = new HashSet<Comment>()
+                },
+            };
+
+            return articles.AsQueryable();
+        }
+    }
+}
--- HealthySystem/Tests/HealthySystem.Web.Controllers.Tests/HomeControllerTests.cs
+++ HealthySystem/Tests/HealthySystem.Web.Controllers.Tests/HomeControllerTests.cs
@@ -1,20 +1,59 @@
 ﻿namespace HealthySystem.Web.Controllers.Tests
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using HealthySystem.Services.Data.Contracts;
     using HealthySystem.Services.Web.Contracts;
+    using HealthySystem.Web.Controllers.Tests.FakeDbRepository;
     using HealthySystem.Web.Infrastructure.Mapping;
+    using HealthySystem.Web.ViewModels;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
+    using TestStack.FluentMVCTesting;
 
     [TestClass]
     public class HomeControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var autoMapperConfig = new AutoMapperConfig();
-            autoMapperConfig.Execute(typeof(HomeController).Assembly);
+        private HomeController controller;
+        private Mock<IArticleService> articleServiceMock;
+        private Mock<ICacheService> cacheServiceMock;
+        private AutoMapperConfig autoMapperConfig;
 
-            var cacheServiceMock = new Mock<ICacheService>();
+        [TestInitialize]
+        public void Init()
+        {
+            this.autoMapperConfig = new AutoMapperConfig();
+            this.autoMapperConfig.Execute(typeof(HomeController).Assembly);
+
+            this.cacheServiceMock = new Mock<ICacheService>();
+            this.cacheServiceMock.Setup(x => x.Get(It.IsAny<string>(), It.IsAny<object>, It.IsAny<int>()))
+                .Returns(ArticleDbRepository.GetArticles());
+
+            this.articleServiceMock = new Mock<IArticleService>();
+            this.articleServiceMock.Setup(x => x.AnyByTitle(It.IsAny<string>())).Returns(false);
+
+            this.controller = new HomeController(this.articleServiceMock.Object)
+            {
+                Cache = this.cacheServiceMock.Object
+            };
         }
+
+        [TestMethod]
+        public void TestHomeControllerShouldRenderDefaultIndex()
+        {
+            this.controller.WithCallTo(x => x.Index()).ShouldRenderDefaultView();
+        }
+
+        //[TestMethod]
+        //public void TestHomeControllerIndexShouldMapDataCorrectly()
+        //{
+        //    this.controller.WithCallTo(x => x.Index())
+        //        .ShouldRenderView("Index")
+        //        .WithModel<ArticleSitePreviewViewModel>(
+        //            vm =>
+        //            {
+        //                Assert.AreEqual(ArticleDbRepository.GetArticles().ToList()[vm.Id - 1], vm.Id);
+        //            }).AndNoModelErrors();
+        //}
     }
 }
